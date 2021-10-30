1d98ba6a33b14dafd400cdee887834ef45e59878
Stella Lok <stellalok@gmail.com>
2021-10-31 02:33:34 +0800

    Change: Implement basic license key generation and verification endpoints

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -3,16 +3,30 @@ import makeServiceWorkerEnv from 'service-worker-mock'
 
 declare var global: any
 
+global.LICENSE_STORE = {
+  get: jest.fn(() => "AAAA-AAAA-AAAA-AAAA"),
+  put: jest.fn(() => "AAAA-AAAA-AAAA-AAAA"),
+};
+
 describe('handle', () => {
   beforeEach(() => {
     Object.assign(global, makeServiceWorkerEnv())
     jest.resetModules()
   })
 
-  test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', { method: 'GET' }))
-    expect(result.status).toEqual(200)
-    const text = await result.text()
-    expect(text).toEqual('request method: GET')
+  test('handle /generate', async () => {
+    const result = await handleRequest(new Request('/generate', { method: 'POST', body: JSON.stringify({ userId: "foo" }), }))
+    expect(result.status).toEqual(200);
+    const resultObj = await result.json();
+    // @ts-ignore
+    expect(resultObj["userId"]).toEqual("foo");
+  })
+
+  test('handle /verify', async () => {
+    const result = await handleRequest(new Request('/verify', { method: 'GET', body: JSON.stringify({ userId: "foo", licenseKey: "AAAA-AAAA-AAAA-AAAA" }), }))
+    expect(result.status).toEqual(200);
+    const resultObj = await result.json();
+    // @ts-ignore
+    expect(resultObj["valid"]).toEqual(true);
   })
 })
